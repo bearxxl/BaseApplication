ee6c5f0db66b80e8884eaf9db22737dd606e1a19
xuelianXiong <14786@etransfar.com>
2019-05-12 16:01:34 +0800

    添加byteutil 添加MD5util

---
+++ encryptlib/src/main/java/base/xuelianx/com/encryptlib/ByteUtil.java
@@ -0,0 +1,90 @@
+package base.xuelianx.com.encryptlib;
+
+
+import android.graphics.Bitmap;
+
+import java.io.ByteArrayOutputStream;
+
+/**
+ * Title: ByteUtil.java
+ * Description: 字节转换工具
+ * Copyright (c) 个人版权所有 2019/5/12
+ * Created DateTime: 2019/5/12 15:35
+ * Created by xuelianXiong.
+ */
+public class ByteUtil {
+    /**
+     *字节数组转成十六进制字符串
+     * @param bArr  待转换的字节数组
+     * @return  字节数组的十六进制字符串
+     */
+    public static  String bytesToHexString(byte[] bArr) {
+        StringBuffer sb = new StringBuffer(bArr.length);
+        String temp;
+        for (byte b : bArr) {
+            temp = Integer.toHexString(b & 0xff);
+            if (temp.length() == 1) {
+                temp = "0" + temp;
+            }
+            sb.append(temp);
+        }
+        return sb.toString();
+    }
+    /**
+     *十六进制字符串转成字节数组
+     * @param sourceStr  待转换的十六进制字符串
+     * @return  字节数组
+     */
+    private static byte[] hexToBytes(String sourceStr)
+    {
+        if (sourceStr == null || sourceStr.length() < 2)
+        {
+            return new byte[0];
+        }
+        sourceStr = sourceStr.toUpperCase();
+        int l = sourceStr.length() / 2;
+        byte[] result = new byte[l];
+        for (int i = 0; i < l; ++i)
+        {
+            String tmp = sourceStr.substring(2 * i, 2 * i + 2);
+            result[i] = (byte)(Integer.parseInt(tmp, 16) & 0xFF);
+        }
+        return result;
+    }
+
+    /**
+     * 16进制转字节
+     * @param hexStr 十六进制字符串
+     * @return  字节
+     * 注意：Hex的字符串必须为十六进制的字符，否则会抛出异常。Hex的范围为0x00到0xFF
+     */
+    public static byte hexToByte(String hexStr){
+        return (byte)Integer.parseInt(hexStr,16);
+    }
+
+    /**
+     * 字节转16进制
+     * @param mByte 待转换字节
+     * @return  16进制字符串
+     */
+    public static String byteToHex(byte mByte){
+        String hex = Integer.toHexString(mByte & 0xFF);
+        if(hex.length() < 2){
+            hex = "0" + hex;
+        }
+        return hex;
+    }
+
+    /**
+     * bitmap转成字节数组
+     * @param bm bitmap
+     * @return 字节数组
+     */
+    public static byte[] bitmapToBytes(Bitmap bm) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bm.compress(Bitmap.CompressFormat.PNG, 100, baos);
+        return baos.toByteArray();
+    }
+
+
+}
---
+++ encryptlib/src/main/java/base/xuelianx/com/encryptlib/MD5Util.java
@@ -0,0 +1,99 @@
+package base.xuelianx.com.encryptlib;
+
+import android.graphics.Bitmap;
+import android.text.TextUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Title: MD5Util.java
+ * Description: MD5加密工具类
+ * Copyright (c) 个人版权所有 2019/5/12
+ * Created DateTime: 2019/5/12 14:28
+ * Created by xuelianXiong.
+ */
+public class MD5Util {
+
+    /**
+     * 文件的32位md5值
+     *
+     * @param sourceFile 文件
+     * @return 文件的md5值
+     */
+    public static String getMD5(File sourceFile) {
+        BigInteger bi = null;
+        try {
+            //设置输入流的缓存大小 字节
+            byte[] buffer = new byte[8192];
+            int len = 0;
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            FileInputStream fis = new FileInputStream(sourceFile);
+            //将整个文件全部读入到加密器中
+            while ((len = fis.read(buffer)) != -1) {
+                md.update(buffer, 0, len);
+            }
+            fis.close();
+            //对读入的数据进行加密
+            byte[] bytes = md.digest();
+            //生成16进制字符串
+            return ByteUtil.bytesToHexString(bytes);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    /**
+     * 字符串的32位md5值
+     *
+     * @param sourceStr 待加密内容
+     * @return 字符串的md5值
+     */
+    public static String getMD5(String sourceStr) {
+        if (TextUtils.isEmpty(sourceStr)) {
+            return "";
+        }
+        MessageDigest md5 = null;
+        try {
+            md5 = MessageDigest.getInstance("MD5");
+            byte[] bytes = md5.digest(sourceStr.getBytes());
+            //生成16进制字符串
+            return ByteUtil.bytesToHexString(bytes);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    /**
+     * 32位md5加盐加密
+     *
+     * @param sourceStr 待加密字符串
+     * @param slat      盐值
+     * @return md5加密后的字符串
+     */
+    public static String getSaltMD5(String sourceStr, String slat) {
+        if (TextUtils.isEmpty(sourceStr)) {
+            return "";
+        }
+        MessageDigest md5 = null;
+        try {
+            md5 = MessageDigest.getInstance("MD5");
+            byte[] bytes = md5.digest((sourceStr + slat).getBytes());
+            //生成16进制字符串
+            return ByteUtil.bytesToHexString(bytes);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+
+}
