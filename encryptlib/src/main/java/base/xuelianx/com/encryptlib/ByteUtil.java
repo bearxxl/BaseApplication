3d57d4afc8af5524534010575cb6b36b68b5c07a
xuelianXiong <14786@etransfar.com>
2019-05-12 16:15:12 +0800

    byteutil添加字节数组转bitmap的方法

--- encryptlib/src/main/java/base/xuelianx/com/encryptlib/ByteUtil.java
+++ encryptlib/src/main/java/base/xuelianx/com/encryptlib/ByteUtil.java
@@ -2,6 +2,7 @@ package base.xuelianx.com.encryptlib;
 
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 
 import java.io.ByteArrayOutputStream;
 
@@ -14,11 +15,12 @@ import java.io.ByteArrayOutputStream;
  */
 public class ByteUtil {
     /**
-     *字节数组转成十六进制字符串
-     * @param bArr  待转换的字节数组
-     * @return  字节数组的十六进制字符串
+     * 字节数组转成十六进制字符串
+     *
+     * @param bArr 待转换的字节数组
+     * @return 字节数组的十六进制字符串
      */
-    public static  String bytesToHexString(byte[] bArr) {
+    public static String bytesToHexString(byte[] bArr) {
         StringBuffer sb = new StringBuffer(bArr.length);
         String temp;
         for (byte b : bArr) {
@@ -30,46 +32,47 @@ public class ByteUtil {
         }
         return sb.toString();
     }
+
     /**
-     *十六进制字符串转成字节数组
-     * @param sourceStr  待转换的十六进制字符串
-     * @return  字节数组
+     * 十六进制字符串转成字节数组
+     *
+     * @param sourceStr 待转换的十六进制字符串
+     * @return 字节数组
      */
-    private static byte[] hexToBytes(String sourceStr)
-    {
-        if (sourceStr == null || sourceStr.length() < 2)
-        {
+    private static byte[] hexToBytes(String sourceStr) {
+        if (sourceStr == null || sourceStr.length() < 2) {
             return new byte[0];
         }
         sourceStr = sourceStr.toUpperCase();
         int l = sourceStr.length() / 2;
         byte[] result = new byte[l];
-        for (int i = 0; i < l; ++i)
-        {
+        for (int i = 0; i < l; ++i) {
             String tmp = sourceStr.substring(2 * i, 2 * i + 2);
-            result[i] = (byte)(Integer.parseInt(tmp, 16) & 0xFF);
+            result[i] = (byte) (Integer.parseInt(tmp, 16) & 0xFF);
         }
         return result;
     }
 
     /**
      * 16进制转字节
+     *
      * @param hexStr 十六进制字符串
-     * @return  字节
+     * @return 字节
      * 注意：Hex的字符串必须为十六进制的字符，否则会抛出异常。Hex的范围为0x00到0xFF
      */
-    public static byte hexToByte(String hexStr){
-        return (byte)Integer.parseInt(hexStr,16);
+    public static byte hexToByte(String hexStr) {
+        return (byte) Integer.parseInt(hexStr, 16);
     }
 
     /**
      * 字节转16进制
+     *
      * @param mByte 待转换字节
-     * @return  16进制字符串
+     * @return 16进制字符串
      */
-    public static String byteToHex(byte mByte){
+    public static String byteToHex(byte mByte) {
         String hex = Integer.toHexString(mByte & 0xFF);
-        if(hex.length() < 2){
+        if (hex.length() < 2) {
             hex = "0" + hex;
         }
         return hex;
@@ -77,6 +80,7 @@ public class ByteUtil {
 
     /**
      * bitmap转成字节数组
+     *
      * @param bm bitmap
      * @return 字节数组
      */
@@ -84,6 +88,21 @@ public class ByteUtil {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         bm.compress(Bitmap.CompressFormat.PNG, 100, baos);
         return baos.toByteArray();
+    }
+
+    /**
+     * 字节数组转成bitmap
+     * @param bytes 待转字节数组
+     * @param opts  可不传
+     * @return  bitmap
+     */
+    public static Bitmap bytesToBytes(byte[] bytes, BitmapFactory.Options opts) {
+        if (bytes != null)
+            if (opts != null)
+                return BitmapFactory.decodeByteArray(bytes, 0, bytes.length, opts);
+            else
+                return BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+        return null;
     }
 
 
